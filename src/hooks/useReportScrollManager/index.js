99e028c9414a192012026ef1a52f47d60452a91b
Aimane Chnaif <96077027+aimane-chnaif@users.noreply.github.com>
2023-10-27 14:00:18 +0100

    Revert "[No QA] [TS Migration] Migrate useReportScrollManager and ReportScreenContext to TypeScript"

--- src/hooks/useReportScrollManager/index.js
+++ src/hooks/useReportScrollManager/index.js
@@ -1,26 +1,29 @@
 import {useContext, useCallback} from 'react';
 import {ActionListContext} from '../../pages/home/ReportScreenContext';
-import ReportScrollManagerData from './types';
 
-function useReportScrollManager(): ReportScrollManagerData {
+function useReportScrollManager() {
     const flatListRef = useContext(ActionListContext);
 
     /**
-     * Scroll to the provided index.
+     * Scroll to the provided index. On non-native implementations we do not want to scroll when we are scrolling because
+     * we are editing a comment.
+     *
+     * @param {Object} index
+     * @param {Boolean} isEditing
      */
-    const scrollToIndex = (index: number) => {
-        if (!flatListRef?.current) {
+    const scrollToIndex = (index, isEditing) => {
+        if (!flatListRef.current || isEditing) {
             return;
         }
 
-        flatListRef.current.scrollToIndex({index});
+        flatListRef.current.scrollToIndex(index);
     };
 
     /**
      * Scroll to the bottom of the flatlist.
      */
     const scrollToBottom = useCallback(() => {
-        if (!flatListRef?.current) {
+        if (!flatListRef.current) {
             return;
         }
 
--- src/hooks/useReportScrollManager/index.native.js
+++ src/hooks/useReportScrollManager/index.native.js
@@ -1,27 +1,27 @@
 import {useContext, useCallback} from 'react';
 import {ActionListContext} from '../../pages/home/ReportScreenContext';
-import ReportScrollManagerData from './types';
 
-function useReportScrollManager(): ReportScrollManagerData {
+function useReportScrollManager() {
     const flatListRef = useContext(ActionListContext);
 
     /**
-     * Scroll to the provided index. On non-native implementations we do not want to scroll when we are scrolling because
-     * we are editing a comment.
+     * Scroll to the provided index.
+     *
+     * @param {Object} index
      */
-    const scrollToIndex = (index: number, isEditing?: boolean) => {
-        if (!flatListRef?.current || isEditing) {
+    const scrollToIndex = (index) => {
+        if (!flatListRef.current) {
             return;
         }
 
-        flatListRef.current.scrollToIndex({index, animated: true});
+        flatListRef.current.scrollToIndex(index);
     };
 
     /**
      * Scroll to the bottom of the flatlist.
      */
     const scrollToBottom = useCallback(() => {
-        if (!flatListRef?.current) {
+        if (!flatListRef.current) {
             return;
         }
 
