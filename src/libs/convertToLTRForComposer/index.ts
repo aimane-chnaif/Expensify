0cb8256ebe87c82083d6b2d838a7b211e4164fb6
Aimane Chnaif <96077027+aimane-chnaif@users.noreply.github.com>
2023-10-29 14:18:30 +0100

    Revert "emoji/mention suggestion are displayed"

--- src/libs/convertToLTRForComposer/index.ts
+++ src/libs/convertToLTRForComposer/index.ts
@@ -1,21 +1,20 @@
 import CONST from '@src/CONST';
 import ConvertToLTRForComposer from './types';
 
-function hasRTLCharacters(text: string): boolean {
-    // Regular expressions to match RTL character ranges.
+function hasLTRorRTLCharacters(text: string): boolean {
+    // Regular expressions to match LTR and RTL character ranges.
+    // eslint-disable-next-line no-control-regex
+    const ltrPattern = /[\u0001-\u05FF\u0600-\u06FF\u0750-\u077F\uFB50-\uFDFF\uFE70-\uFEFF]/;
     const rtlPattern = /[\u0591-\u07FF\uFB1D-\uFDFD\uFE70-\uFEFC]/;
-    return rtlPattern.test(text);
+
+    return ltrPattern.test(text) || rtlPattern.test(text);
 }
 
 // Converts a given text to ensure it starts with the LTR (Left-to-Right) marker.
 const convertToLTRForComposer: ConvertToLTRForComposer = (text) => {
-    // Ensure that the text starts with RTL characters if not we return the same text to avoid concatination with special character at the start which leads to unexpected behaviour for Emoji/Mention suggestions.
-    if (!hasRTLCharacters(text)) {
-        // If text is empty string return empty string to avoid an empty draft due to special character.
-        if (text === '' || CONST.UNICODE.LTR.match(text)) {
-            return '';
-        }
-        return text;
+    // Ensure the text contains LTR or RTL characters to avoid an unwanted special character at the beginning, even after a backspace deletion.
+    if (!hasLTRorRTLCharacters(text)) {
+        return '';
     }
 
     // Check if the text contains only spaces. If it does, we do not concatenate it with CONST.UNICODE.LTR,
